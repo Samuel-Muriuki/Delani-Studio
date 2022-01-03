50f1881ac109d0a113a037be0b490e809f8399c0
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-01-03 13:28:09 +0300

    It will add the submit button response

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-$(document).ready(function() {
-    $(".hover").hover(function() {
-        $("#design").toggle();
-        $("#designImg").toggle();
-    });
+$(document).ready(function () {
+  $(".hover").hover(function () {
+    $("#design").toggle();
+    $("#designImg").toggle();
+  });
 });
 
-$(document).ready(function() {
-    $(".hover1").hover(function() {
-        $("#development").toggle();
-        $("#developmentImg").toggle();
-    });
+$(document).ready(function () {
+  $(".hover1").hover(function () {
+    $("#development").toggle();
+    $("#developmentImg").toggle();
+  });
 });
 
-$(document).ready(function() {
-    $(".hover2").hover(function() {
-        $("#productManagement").toggle();
-        $("#productManagementImg").toggle();
-    });
+$(document).ready(function () {
+  $(".hover2").hover(function () {
+    $("#productManagement").toggle();
+    $("#productManagementImg").toggle();
+  });
+});
+
+jQuery("button").click(function () {
+  alert("Your response has been received");
 });
