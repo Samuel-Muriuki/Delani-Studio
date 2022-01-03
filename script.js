a05bf67a7c5fec2d9a0d9e87e16acf525b04fe2c
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-01-03 13:26:49 +0300

    It will add the hover effect

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(".hover").hover(function() {
+        $("#design").toggle();
+        $("#designImg").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $(".hover1").hover(function() {
+        $("#development").toggle();
+        $("#developmentImg").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $(".hover2").hover(function() {
+        $("#productManagement").toggle();
+        $("#productManagementImg").toggle();
+    });
+});
