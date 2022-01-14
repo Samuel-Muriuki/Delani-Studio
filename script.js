dc37a2a4582c134dbacf531aa8bd0fce8c4752a4
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-01-14 14:41:19 +0300

    It will rectify the page

--- script.js
+++ script.js
@@ -19,14 +19,82 @@ $(document).ready(function () {
   });
 });
 
-jQuery("button").click(function () {
-  alert("Your response has been received");
+// The Portfolio Hover Effect
+
+$(document).ready(function () {
+  $("#imgHov1").mouseover(function () {
+    $("#textHov1").show();
+  });
+  $("#imgHov1").mouseout(function () {
+    $("#textHov1").hide();
+  });
 });
 
 
 $(document).ready(function () {
-  $(".hov1").hover(function () {
-    $("#imgHov1").toggle();
-    $("#pHov1").toggle();
+  $("#imgHov2").mouseover(function () {
+    $("#textHov2").show();
+  });
+  $("#imgHov2").mouseout(function () {
+    $("#textHov2").hide();
   });
 });
+
+$(document).ready(function () {
+  $("#imgHov3").mouseover(function () {
+    $("#textHov3").show();
+  });
+  $("#imgHov3").mouseout(function () {
+    $("#textHov3").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#imgHov4").mouseover(function () {
+    $("#textHov4").show();
+  });
+  $("#imgHov4").mouseout(function () {
+    $("#textHov4").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#imgHov5").mouseover(function () {
+    $("#textHov5").show();
+  });
+  $("#imgHov5").mouseout(function () {
+    $("#textHov5").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#imgHov6").mouseover(function () {
+    $("#textHov6").show();
+  });
+  $("#imgHov6").mouseout(function () {
+    $("#textHov6").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#imgHov7").mouseover(function () {
+    $("#textHov7").show();
+  });
+  $("#imgHov7").mouseout(function () {
+    $("#textHov7").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#imgHov8").mouseover(function () {
+    $("#textHov8").show();
+  });
+  $("#imgHov8").mouseout(function () {
+    $("#textHov8").hide();
+  });
+});
+//  validation
+$("button").click(function () {
+  alert("Thank you, your message has been response.");
+});
+
